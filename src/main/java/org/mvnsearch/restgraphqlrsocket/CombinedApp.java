c941eeae58ac0ec39231e4f3dad389932e308146
linux_china <libing.chen@gmail.com>
2021-09-02 17:23:59 -0700

    CombinedApp

---
+++ src/main/java/org/mvnsearch/restgraphqlrsocket/CombinedApp.java
@@ -0,0 +1,13 @@
+package org.mvnsearch.restgraphqlrsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombinedApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CombinedApp.class, args);
+    }
+
+}
