2f502a2038ee49964b0c7710a168cc8ab96caf2d
linux_china <libing.chen@gmail.com>
2021-09-02 17:23:42 -0700

    Author

---
+++ src/main/java/org/mvnsearch/restgraphqlrsocket/domain/model/Author.java
@@ -0,0 +1,41 @@
+package org.mvnsearch.restgraphqlrsocket.domain.model;
+
+
+public class Author {
+    private String id;
+    private String firstName;
+    private String lastName;
+
+    public Author() {
+    }
+
+    public Author(String id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
